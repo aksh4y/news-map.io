fd3e3cf930901c0519d4ecdfc7b287b8cef54a4a
Pooneet Thaper <pooneetthaper@gmail.com>
2018-09-30 02:05:10 -0400

    Added the ability to use the Watson Natural Language Understanding API for sentiment using url

---
+++ watson.py
@@ -0,0 +1,35 @@
+import json
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+from watson_developer_cloud.natural_language_understanding_v1 \
+  import Features, KeywordsOptions
+import os
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+  username=os.environ['watson_username'],
+  password=os.environ['watson_password'],
+  version='2018-03-16')
+
+def get_sentiment(url):
+    keywords = natural_language_understanding.analyze(
+      url=url,
+      features=Features(
+        keywords=KeywordsOptions(
+          sentiment=True,
+          emotion=True,
+          limit=200)))['keywords']
+
+    sentiment = 0
+    relevance = 0
+    for keyword_dict in keywords:
+        keyword_relevance = keyword_dict['relevance']**(2)
+        sentiment += (keyword_dict['sentiment']['score'] * keyword_relevance)
+        relevance += keyword_relevance
+        print(keyword_dict['relevance'], '\t', keyword_dict['sentiment']['score'], '\t', keyword_dict['text'])
+
+    return sentiment/relevance
+
+
+if __name__ == '__main__':
+    url = 'https://www.usatoday.com/story/tech/talkingtech/2018/02/19/erica-humanoid-robot-chatty-but-still-has-lot-learn/352281002/'
+    sentiment = get_sentiment(url)
+    print(sentiment)
