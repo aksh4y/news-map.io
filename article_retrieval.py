84e232e0f3d8f0dca5913309480f3e72f1e1bd44
Jane Chen <jane.chen@macaulay.cuny.edu>
2018-09-29 23:24:52 -0400

    retrieved complete contents for nytimes articles

--- article_retrieval.py
+++ article_retrieval.py
@@ -2,9 +2,11 @@ from newsapi import NewsApiClient
 import pandas as pd
 
 df_articles = pd.DataFrame(data={'url': [],
+                                 'url_image': [],
                                  'article': [],
                                  'datetime': [],
-                                 'news_source': []
+                                 'news_source': [],
+                                 'content': []
                                  })
 
 df_locations = pd.DataFrame(data={'location': [],
@@ -16,7 +18,7 @@ df_articles_locations = pd.DataFrame(data={'url': [],
                                            'location': []
                                            })
 
-newsapi = NewsApiClient(api_key='e9c060f2e421497dbb5e6218b18b4b50')
+newsapi = NewsApiClient(api_key='c0c364cd6be242369926fbc245d6865f')
 sources = newsapi.get_sources(language='en')
 
 english_sources_ids = ['abc-news', 'al-jazeera-english', 'associated-press', 'bbc-news', 'bloomberg', 'business-insider', 'buzzfeed', 'cbs-news', 'cnn', 'engadget', 'financial-times', 'fortune', 'fox-news', 'google-news', 'hacker-news', 'independent', 'msnbc', 'national-geographic', 'nbc-news', 'new-york-magazine', 'nhl-news', 'reuters', 'techcrunch', 'techradar', 'the-economist', 'the-guardian-uk', 'the-huffington-post', 'the-new-york-times', 'the-verge', 'the-wall-street-journal', 'the-washington-post', 'time', 'usa-today', 'wired']
@@ -29,10 +31,15 @@ for source in english_sources_ids:
                                         sort_by='popularity')
 
     for article in everything['articles']:
+        if article['content']:
+            truncated_content = article['content'].split('\u2026')[0]
+
         df_articles = df_articles.append({'url': article['url'],
+                                          'url_image': article['url_to_image']
                                           'article': article['title'],
                                           'datetime': article['publishedAt'],
-                                          'news_source': article['source']['id']
+                                          'news_source': article['source']['id'],
+                                          'content': truncated_content
                                           }, ignore_index=True)
 
 df_articles.to_json('df_articles.json', orient='records')
