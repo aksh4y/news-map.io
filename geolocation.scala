40b6a28da07264d26ef471e6d68a215a40c4b25b
Pooneet Thaper <pooneetthaper@gmail.com>
2018-09-30 08:31:19 -0400

    Added geolocation data and updated the geolocation spark script for the data

--- geolocation.scala
+++ geolocation.scala
@@ -1,12 +1,12 @@
 import org.apache.spark.sql._
 
-val dataPath = "data.csv"
-val articlesPath = "articles.csv"
+val dataPath = "geodata/data.parquet.gzip"
+val articlesPath = "geodata/articles.csv"
 
-val data = spark.read.option("header", "true").csv(dataPath)
+val data = spark.read.option("header", "true").parquet(dataPath)
 val articles = spark.read.option("header", "true").csv(articlesPath)
 
 
 val output = articles.join(data, Seq("string"), "inner")
 output.show(20, False)
-spark.write.csv("out.csv")
+spark.write.csv("data/out.csv")
